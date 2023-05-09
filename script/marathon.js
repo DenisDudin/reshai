525ecc71b1db085bc2e24bf464a1fcdd1d1c3d8e
Denis Dudin <denisdudin@MacBook-Pro-Denis.local>
2023-05-09 19:49:27 +0300

    completed project without sending form

--- script/marathon.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll(".lesson").forEach((lesson) => {
-  lesson.addEventListener("click", (e) => {
-    lesson.querySelector(".lesson__text").classList.toggle("show-lesson");
-  });
-});
